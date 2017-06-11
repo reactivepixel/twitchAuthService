11ac9eb3d6e8405d5dce03d5af5ed5d8c43671a7
Chris Chapman <Chapman@apextion.com>
2017-06-11 16:43:55 -0400

    absolute zero hello world

--- src/server.js
+++ src/server.js
@@ -1,66 +1,67 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const exphbs  = require('express-handlebars');
-const cookie_parser = require('cookie-parser');
-const cookie_session = require('cookie-session');
-const passport = require('passport');
-const twitchStrategy = require('passport-twitch').Strategy;
+// const exphbs  = require('express-handlebars');
+// const cookie_parser = require('cookie-parser');
+// const cookie_session = require('cookie-session');
+// const passport = require('passport');
+// const twitchStrategy = require('passport-twitch').Strategy;
 
-const User = require('./models/db').user;
+// const User = require('./models/db').user;
 
 const port = process.env.PORT | 3000;
 
 var app = express();
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+//
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
 
 // Middleware
-app.use(body_parser.urlencoded({ extended: true }));
-app.use(cookie_parser());
-app.use(cookie_session({secret:"dontchawanna"}));
-app.use(passport.initialize());
-app.use(express.static("./public"));
+// app.use(body_parser.urlencoded({ extended: true }));
+// app.use(cookie_parser());
+// app.use(cookie_session({secret:"dontchawanna"}));
+// app.use(passport.initialize());
+// app.use(express.static("./public"));
 
+//
+// passport.use(new twitchStrategy({
+//     clientID: process.env.TWITCH_CLIENT_ID,
+//     clientSecret: process.env.TWITCH_CLIENT_SECRET,
+//     callbackURL: "https://twitch-auth-service-production.herokuapp.com/auth/twitch/callback",
+//     scope: "user_read"
+//   },
+//   (accessToken, refreshToken, profile) => {
+//     // Suppose we are using mongo..
+//     // User.findOrCreate({ twitchId: profile.id }, function (err, user) {
+//     //   return done(err, user);
+//     // });
+//     console.log('Debug: accessToken', accessToken);
+//     console.log('Debug: refreshToken', refreshToken);
+//     console.log('Debug: Profile', profile);
+//   }
+// ));
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+// });
+//
+// passport.deserializeUser(function(user, done) {
+//     done(null, user);
+// });
 
-passport.use(new twitchStrategy({
-    clientID: "4gzbq395n98t5s64sfwpmx9dxo2anm",
-    clientSecret: "i4asxasyab1ml8zfl6m86g9994j8jq",
-    callbackURL: "https://twitch-auth-service-production.herokuapp.com/auth/twitch/callback",
-    scope: "user_read"
-  },
-  (accessToken, refreshToken, profile) => {
-    // Suppose we are using mongo..
-    // User.findOrCreate({ twitchId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-    console.log('Debug: accessToken', accessToken);
-    console.log('Debug: refreshToken', refreshToken);
-    console.log('Debug: Profile', profile);
-  }
-));
-passport.serializeUser(function(user, done) {
-    done(null, user);
+app.get("/", (req, res) => {
+    //res.render('home');
+    res.json({ status: true });
 });
+//
+// app.get("/auth/twitch", passport.authenticate("twitch"), (req, res) => {
+//   res.json({nailed: 'it'})
+// });
+//
+// app.get("/auth/twitch/callback", passport.authenticate("twitch", { failureRedirect: "/" }), function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect("/");
+// });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-app.get("/", function (req, res) {
-    res.render('home');
-});
-
-app.get("/auth/twitch", passport.authenticate("twitch"), (req, res) => {
-  res.json({nailed: 'it'})
-});
-
-app.get("/auth/twitch/callback", passport.authenticate("twitch", { failureRedirect: "/" }), function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("/");
-});
-
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log('Server up and running on port', port);
 });
 
